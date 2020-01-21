fa08bc3a3a981c309c7a10ab08d63855c5e16891
besifear <besnik.korca1@gmail.com>
2020-01-21 08:47:31 +0100

    Add custom fonts and use Store in App.js

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import StoreWrapper from './components/hoc/StoreWrapper';
+import ForumNavigator from './navigation/ForumNavigator';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  const handleSetFontLoaded = () => setFontsLoaded(true);
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={handleSetFontLoaded}
+      />
+    );
+  }
+
+  return (
+    <StoreWrapper>
+      <ForumNavigator />
+    </StoreWrapper>
+  );
+}
