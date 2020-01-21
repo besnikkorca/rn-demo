98ed5adb55ebb97a416e06493e217a09f528d015
besifear <besnik.korca1@gmail.com>
2020-01-21 08:45:21 +0100

    Add stack navigation for posts and post detail screen

---
+++ navigation/ForumNavigator.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import PostsListScreen from '../screens/PostsListScreen';
+import Colors from '../constants/Colors';
+import PostDetailScreen from '../screens/PostDetailScreen';
+
+const PostsNavigator = createStackNavigator(
+  {
+    Posts: PostsListScreen,
+    PostDetail: PostDetailScreen,
+  },
+  {
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+    },
+    headerTitleStyle: {
+      fontFamily: 'open-sans-bold'
+    },
+    headerBackTitleStyle: {
+      fontFamily: 'open-sans'
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+  }
+);
+
+export default createAppContainer(PostsNavigator);
